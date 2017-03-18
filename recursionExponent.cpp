4c849bb80153cc2e889292f2b2598277c25680db
shuklam20 <shuklam@seas.upenn.edu>
2017-03-18 17:10:15 -0400

    Created recursionExponent.cpp
    Calculates n^p using recursions.

---
+++ recursionExponent.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int exponential(int n, int p)
+{
+	if (p <= 1) return n;
+	else return n*exponential(n, p - 1);
+}
+
+int main()
+{
+  int n, p;
+  cout << "Enter the number:" << endl;
+  cin >> n;
+  cout << "Enter the exponent that you want:" << endl;
+  cin >> p;
+  cout << n << "^" << p << " is " << exponential(n, p) <<endl;
+}

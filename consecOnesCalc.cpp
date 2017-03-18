6e56b4068c8a7ab0764bb70b1ca6e3c38ca256a0
shuklam20 <shuklam@seas.upenn.edu>
2017-03-18 19:25:58 -0400

    Created consecOnesCalc.cpp
    Calculates consecutive 1s in the binary representation of a number

---
+++ consecOnesCalc.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int n, bin;
+	cout << "enter the number:" << endl;
+	cin >> n;
+	cout << "binary for " << n << " is ";
+  
+	while (n > 0)
+	{
+		bin = n % 2;
+		n = n / 2;		
+		cout << bin; // bin outputs the reminder with two, (hence the binary representation)
+		if (bin == 1) {
+			s++; total++; // s is same as "total" except that it resets to zero if there is a zero in bin representation
+			if (s >= t) t = s;
+		}
+		else s = 0;
+	}
+	cout << endl;
+	cout << "Total 1s are: " << total << endl;
+	cout << "Consecutive 1s are: " << t << endl;
+  
+  return 0;
+}

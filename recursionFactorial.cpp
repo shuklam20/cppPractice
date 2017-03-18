c55ecddc427e7175459f0e2d95974f91385539d9
shuklam20 <shuklam@seas.upenn.edu>
2017-03-18 17:07:23 -0400

    added header file and cout

--- recursionFactorial.cpp
+++ recursionFactorial.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include "stdafx.h"
+
+using namespace std;
 
 int factorial(int n)
 {
     if(n<=0) return 1;
     else return n*factorial(n-1);
-  
 }
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     int n; 
+    cout << "enter the number:" <<endl;
     cin >> n;
     cout << factorial(n);
     return 0;
